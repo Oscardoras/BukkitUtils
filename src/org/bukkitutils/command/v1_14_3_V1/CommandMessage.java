662ef1a8ca0221b3a5dca4c75f25e89809150df6
oscar <oscar@oscar-PC>
2019-07-02 17:00:30 +0200

    Patch.

--- src/org/bukkitutils/command/v1_14_3_V1/CommandMessage.java
+++ src/org/bukkitutils/command/v1_14_3_V1/CommandMessage.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Properties;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.GameRule;
 import org.bukkit.World;
 import org.bukkit.command.BlockCommandSender;
@@ -18,6 +17,9 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkitutils.io.DataFile;
 import org.bukkitutils.io.TranslatableMessage;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TranslatableComponent;
 
 /** Contains methods to broadcast command messages */
 public final class CommandMessage {
@@ -47,33 +49,39 @@ public final class CommandMessage {
 	/**
 	 * Broadcasts a command translatable message if it have to.
 	 * @param sender the CommandSender who performed this command
+	 * @param broadcast if the message needs to be broadcast
 	 * @param message the TranslatableMessage object representing the translatable message to broadcast
 	 * @param args the arguments for the translatable message
 	 */
-	public static void send(CommandSender sender, TranslatableMessage message, String... args) {
+	public static void send(CommandSender sender, boolean broadcast, TranslatableMessage message, String... args) {
 		CommandSender caller = sender instanceof ProxiedCommandSender ? ((ProxiedCommandSender) sender).getCaller() : sender;
 		World world = Bukkit.getWorlds().get(0);
 		
 		if (caller instanceof BlockCommandSender) {
 			caller.sendMessage(message.getMessage(caller, args));
+			if (broadcast)
     		if (world.getGameRuleValue(GameRule.COMMAND_BLOCK_OUTPUT)) {
     			broadcastToConsole(caller, world, message, args);
     			broadcastToOps(caller, world, message, args);
     		}
 		} else if (caller instanceof Entity) {
 			String msg = message.getMessage(caller, args);
-			if (msg.startsWith(ChatColor.RED + "") || world.getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK)) caller.sendMessage(msg);
-			broadcastToConsole(caller, world, message, args);
-			broadcastToOps(caller, world, message, args);
+			if (!broadcast || world.getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK)) caller.sendMessage(msg);
+			if (broadcast) {
+				broadcastToConsole(caller, world, message, args);
+				broadcastToOps(caller, world, message, args);
+			}
 		} else if (caller instanceof ConsoleCommandSender) {
 			if (!(sender instanceof ProxiedCommandSender) || world.getGameRuleValue(GameRule.COMMAND_BLOCK_OUTPUT)) {
 				caller.sendMessage(ChatColor.stripColor(message.getMessage(caller, args)));
-			    if (broadcastConsoleToOps) broadcastToOps(caller, world, message, args);
+				if (broadcast) if (broadcastConsoleToOps) broadcastToOps(caller, world, message, args);
 			}
 		} else if (caller instanceof RemoteConsoleCommandSender) {
 			caller.sendMessage(message.getMessage(caller, args));
-			broadcastToConsole(caller, world, message, args);
-			if (broadcastRconToOps) broadcastToOps(caller, world, message, args);
+			if (broadcast) {
+				broadcastToConsole(caller, world, message, args);
+				if (broadcastRconToOps) broadcastToOps(caller, world, message, args);
+			}
 		}
 	}
 	
@@ -82,18 +90,34 @@ public final class CommandMessage {
 			CommandSender callee = sender instanceof ProxiedCommandSender ? ((ProxiedCommandSender) sender).getCallee() : sender;
 			CommandSender console = Bukkit.getConsoleSender();
 			String msg = message.getMessage(console, args);
-			if (!msg.startsWith(ChatColor.RED + "")) console.sendMessage(ChatColor.stripColor("[" + callee.getName() + ": " + msg + "]"));
+			String name = callee instanceof ConsoleCommandSender ? "Server" : callee.getName();
+			
+			TranslatableComponent component = new TranslatableComponent("chat.type.admin");
+			component.addWith(name);
+			component.addWith(msg);
+			component.setColor(ChatColor.GRAY);
+			component.setItalic(true);
+			
+			console.spigot().sendMessage(component);
 		}
 	}
 	
 	private static void broadcastToOps(CommandSender sender, World world, TranslatableMessage message, String... args) {
 		CommandSender caller = sender instanceof ProxiedCommandSender ? ((ProxiedCommandSender) sender).getCaller() : sender;
 		CommandSender callee = sender instanceof ProxiedCommandSender ? ((ProxiedCommandSender) sender).getCallee() : sender;
+		String name = callee instanceof ConsoleCommandSender ? "Server" : callee.getName();
 		if (world.getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK)) {
 			for (Player other : Bukkit.getOnlinePlayers()) {
 				if (other.hasPermission("bukkit.broadcast.scradmin") && !other.equals(caller)) {
 					String msg = message.getMessage(other, args);
-					if (!msg.startsWith(ChatColor.RED + "")) other.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "[" + callee.getName() + ": " + msg + "]");
+					
+					TranslatableComponent component = new TranslatableComponent("chat.type.admin");
+					component.addWith(name);
+					component.addWith(msg);
+					component.setColor(ChatColor.GRAY);
+					component.setItalic(true);
+					
+					other.spigot().sendMessage(component);
 				}
 			}
 	    }
@@ -132,12 +156,15 @@ public final class CommandMessage {
 		World world = Bukkit.getWorlds().get(0);
 		
 		for (String message : messages) {
-			if (caller instanceof BlockCommandSender) caller.sendMessage(message);
-			else if (caller instanceof Entity)
-				if (message.startsWith(ChatColor.RED + "") || world.getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK)) caller.sendMessage(message);
-			else if (caller instanceof ConsoleCommandSender)
+			if (caller instanceof BlockCommandSender) {
+				caller.sendMessage(message);
+			} else if (caller instanceof Entity) {
+				if (world.getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK)) caller.sendMessage(message);
+			} else if (caller instanceof ConsoleCommandSender) {
 				if (!(sender instanceof ProxiedCommandSender) || world.getGameRuleValue(GameRule.COMMAND_BLOCK_OUTPUT)) caller.sendMessage(ChatColor.stripColor(message));
-			else if (caller instanceof RemoteConsoleCommandSender) caller.sendMessage(message);
+			} else if (caller instanceof RemoteConsoleCommandSender) {
+				caller.sendMessage(message);
+			}
 		}
 	}
 	
